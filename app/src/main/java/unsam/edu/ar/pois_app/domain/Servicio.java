952c1f675141ada5bcbc6520088c0ebf92b2ffe5
dwernerx <dwerner730@gmail.com>
2016-11-16 00:47:32 -0300

    --

--- app/src/main/java/unsam/edu/ar/pois_app/domain/Servicio.java
+++ app/src/main/java/unsam/edu/ar/pois_app/domain/Servicio.java
@@ -1,6 +1,8 @@
 package unsam.edu.ar.pois_app.domain;
 
-public class Servicio {
+import java.io.Serializable;
+
+public class Servicio implements Serializable{
     private String nombre;
     private String horario;
 
